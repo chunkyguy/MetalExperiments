56978cef0afd7ce5c262f849327302c89a13b981
Sidharth Juyal <juyal.sidharth@gmail.com>
2020-04-22 00:40:24 +0200

    Render triangle

--- MetalExperiments Shared/Shaders.metal
+++ MetalExperiments Shared/Shaders.metal
@@ -8,44 +8,19 @@
 #include <metal_stdlib>
 #include <simd/simd.h>
 
-// Including header shared between this Metal shader code and Swift/C code executing Metal API commands
-#import "ShaderTypes.h"
-
 using namespace metal;
 
-typedef struct
+struct Vertex {
+  float4 position [[position]];
+  float4 color;
+};
+
+vertex Vertex vert_main(device Vertex *vertices[[buffer(0)]], uint vid [[vertex_id]])
 {
-    float3 position [[attribute(VertexAttributePosition)]];
-    float2 texCoord [[attribute(VertexAttributeTexcoord)]];
-} Vertex;
-
-typedef struct
-{
-    float4 position [[position]];
-    float2 texCoord;
-} ColorInOut;
-
-vertex ColorInOut vertexShader(Vertex in [[stage_in]],
-                               constant Uniforms & uniforms [[ buffer(BufferIndexUniforms) ]])
-{
-    ColorInOut out;
-
-    float4 position = float4(in.position, 1.0);
-    out.position = uniforms.projectionMatrix * uniforms.modelViewMatrix * position;
-    out.texCoord = in.texCoord;
-
-    return out;
+  return vertices[vid];
 }
 
-fragment float4 fragmentShader(ColorInOut in [[stage_in]],
-                               constant Uniforms & uniforms [[ buffer(BufferIndexUniforms) ]],
-                               texture2d<half> colorMap     [[ texture(TextureIndexColor) ]])
+fragment float4 frag_main(Vertex v [[stage_in]])
 {
-    constexpr sampler colorSampler(mip_filter::linear,
-                                   mag_filter::linear,
-                                   min_filter::linear);
-
-    half4 colorSample   = colorMap.sample(colorSampler, in.texCoord.xy);
-
-    return float4(colorSample);
+  return v.color;
 }
--- MetalExperiments macOS/WLRenderView.m
+++ MetalExperiments macOS/WLRenderView.m
@@ -6,17 +6,41 @@
 #import "WLRenderView.h"
 #import <Quartz/Quartz.h>
 #import <Metal/Metal.h>
+#import <simd/simd.h>
+
+typedef struct _WLVertex {
+  vector_float4 position;
+  vector_float4 color;
+} WLVertex;
 
 @interface WLRenderView ()
 {
   CAMetalLayer *_metalLayer;
   id<MTLDevice> _device;
+  id<MTLCommandQueue> _cmdQueue;
+  id<MTLBuffer> _buffer;
+  id<MTLLibrary> _lib;
+  MTLPixelFormat _pixelFormat;
+  id<MTLRenderPipelineState> _pipeline;
 }
 @end
 
 @implementation WLRenderView
 
 - (void)setUp
+{
+
+  _pixelFormat = MTLPixelFormatBGRA8Unorm;
+
+  [self setUpDevice];
+  [self setUpBuffers];
+  [self setUpPipeline];
+
+
+  _cmdQueue = [_device newCommandQueue];
+}
+
+- (void)setUpDevice
 {
   [self setWantsLayer:YES];
   _metalLayer = [CAMetalLayer layer];
@@ -25,7 +49,31 @@
 
   _device = MTLCreateSystemDefaultDevice();
   _metalLayer.device = _device;
-  _metalLayer.pixelFormat = MTLPixelFormatBGRA8Unorm;
+  _metalLayer.pixelFormat = _pixelFormat;
+}
+
+- (void)setUpBuffers
+{
+  WLVertex vertices[] = {
+    {.position = {0.0f, 0.5f, 0.0f, 1.0f}, .color = {1.0f, 0.0f, 0.0f, 1.0f}},
+    {.position = {-0.5f, -0.5f, 0.0f, 1.0f}, .color = {0.0f, 1.0f, 0.0f, 1.0f}},
+    {.position = {0.5f, -0.5f, 0.0f, 1.0f}, .color = {0.0f, 0.0f, 1.0f, 1.0f}},
+  };
+
+  _buffer = [_device newBufferWithBytes:vertices length:sizeof(vertices) options:MTLResourceOptionCPUCacheModeDefault];
+}
+
+- (void)setUpPipeline
+{
+  _lib = [_device newDefaultLibrary];
+  id<MTLFunction> vertFunc = [_lib newFunctionWithName:@"vert_main"];
+  id<MTLFunction> fragFunc = [_lib newFunctionWithName:@"frag_main"];
+
+  MTLRenderPipelineDescriptor *pipeDesc = [[MTLRenderPipelineDescriptor alloc] init];
+  pipeDesc.vertexFunction = vertFunc;
+  pipeDesc.fragmentFunction = fragFunc;
+  pipeDesc.colorAttachments[0].pixelFormat = _pixelFormat;
+  _pipeline = [_device newRenderPipelineStateWithDescriptor:pipeDesc error:nil];
 }
 
 - (void)redraw
@@ -33,19 +81,18 @@
   id<CAMetalDrawable> drawable = [_metalLayer nextDrawable];
   id<MTLTexture> texture = drawable.texture;
 
+
+  id<MTLCommandBuffer> cmdBuf = [_cmdQueue commandBuffer];
+
   MTLRenderPassDescriptor *passDesc = [MTLRenderPassDescriptor renderPassDescriptor];
   passDesc.colorAttachments[0].texture = texture;
   passDesc.colorAttachments[0].loadAction = MTLLoadActionClear;
   passDesc.colorAttachments[0].storeAction = MTLStoreActionStore;
-  passDesc.colorAttachments[0].clearColor = MTLClearColorMake(1, 0, 0, 1);
-
-  id<MTLCommandQueue> cmdQ = [_device newCommandQueue]; // not cheap
-
-  id<MTLCommandBuffer> cmdBuf = [cmdQ commandBuffer];
-
-  // .. draw calls
-
+  passDesc.colorAttachments[0].clearColor = MTLClearColorMake(0.5, 0.5, 0.5, 1);
   id<MTLRenderCommandEncoder> cmdEnc = [cmdBuf renderCommandEncoderWithDescriptor:passDesc];
+  [cmdEnc setRenderPipelineState:_pipeline];
+  [cmdEnc setVertexBuffer:_buffer offset:0 atIndex:0];
+  [cmdEnc drawPrimitives:MTLPrimitiveTypeTriangle vertexStart:0 vertexCount:3];
   [cmdEnc endEncoding];
 
   [cmdBuf presentDrawable:drawable];
